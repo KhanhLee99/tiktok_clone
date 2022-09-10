05fdfa8fa1be0edc5d11edbbc7c9ad694d0d7a74
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2022-09-10 11:00:01 +0700

    config router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
   return (
-    <div className="App">
-      <h1>REACT APP</h1>
-    </div>
+    <Router>
+      <Routes>
+        {publicRoutes.map((route, index) => {
+          const Layout = route.layout
+            ? route.layout
+            : route.layout === null
+            ? Fragment
+            : DefaultLayout;
+          const Page = route.component;
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+import Following from './Following';
+import Profile from './Profile';
+import Upload from './Upload';
+
+export { Home, Following, Profile, Upload };
