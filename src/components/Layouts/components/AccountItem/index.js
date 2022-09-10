6491c22228451eb4a2347e08c23517fe4c01f3e9
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2022-09-11 00:21:55 +0700

    install tippyjs/react, create popper account result

---
+++ src/components/Layouts/components/AccountItem/index.js
@@ -0,0 +1,28 @@
+import classNames from 'classnames/bind';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+const AccountItem = () => {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/31f33b8da548b6fb59e42400fb9d3f98.jpeg?x-expires=1663002000&x-signature=dNqWJwZKVS9PMUbdnoctdAzpw3w%3D"
+        alt="Hoaa"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+};
+
+export default AccountItem;
